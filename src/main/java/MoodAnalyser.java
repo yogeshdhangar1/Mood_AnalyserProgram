03b67230bdec7bc8d8fec9573dbf62e63be7ada9
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-10-30 07:26:40 -0700

    Added: WelCome To Mood Analyser Program

---
+++ src/main/java/MoodAnalyser.java
@@ -0,0 +1,11 @@
+public class MoodAnalyser {
+    public String mood(){
+        return "Welcome To Mood Analyser";
+
+    }
+
+    public static void main(String[] args) {
+        MoodAnalyser obj = new MoodAnalyser();
+        System.out.println(obj.mood());
+    }
+}
